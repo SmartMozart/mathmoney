1186729abf343da7bed5d04990f4d10356332834
Zachary Hales <51687053+SmartMozart@users.noreply.github.com>
2023-05-27 12:00:39 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+var answer = document.getElementById("answer");
+var question = document.getElementById("question");
+var probAns = null;
+
+window.onload = init;
+
+answer.addEventListener("keydown", function(event) {
+	if (event.key === "Enter") {
+		onEnter();
+	}
+});
+
+answer.addEventListener("keydown", function(event) {
+	if (event.key === "r") {
+		localStorage.setItem('correct', 0);
+		location.reload();
+	}
+});
+
+function onEnter() {
+	let ans = answer.value;
+	if (probAns.toString() == ans) {
+		incCorrect();
+		newQuestion();
+		answer.value = '';
+	}
+}
+
+function init() {
+	newQuestion();
+	let current = localStorage.getItem('correct');
+	if (current == null) {
+		current = 0;
+		localStorage.setItem('correct', 0);
+	}
+}
+
+function newQuestion() {
+	let a = Math.floor(Math.random()*15);
+	let b = Math.floor(Math.random()*15);
+	probAns = a * b;
+	question.innerText = a.toString() + ' x ' + b.toString();
+}
+
+function incCorrect() {
+	let current = localStorage.getItem('correct');
+	current = parseInt(current)+1
+	localStorage.setItem('correct', current);
+}
